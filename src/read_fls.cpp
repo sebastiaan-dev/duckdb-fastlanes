5406fce0bf8b0eafdfffd9cb43dab13ab884b38e
Sebastiaan Gerritsen <dev@sebastiaan.io>
2025-09-27 23:47:14 +0200

    Support all filters in the extension, enable filter pruning and filter pushdown.

--- src/read_fls.cpp
+++ src/read_fls.cpp
@@ -6,11 +6,28 @@
 
 namespace duckdb {
 
+static bool FastLanesScanPushdownExpression(ClientContext& context, const LogicalGet& get, Expression& expr) {
+	return true;
+}
+
+vector<column_t> FastLanesGetRowIdColumns(ClientContext& context, optional_ptr<FunctionData> bind_data) {
+	vector<column_t> result;
+	result.emplace_back(MultiFileReader::COLUMN_IDENTIFIER_FILE_INDEX);
+	result.emplace_back(MultiFileReader::COLUMN_IDENTIFIER_FILE_ROW_NUMBER);
+	return result;
+}
+
 void ReadFastLanes::Register(DatabaseInstance& db) {
 	MultiFileFunction<FastLanesMultiFileInfo> fn("read_fls");
+	fn.named_parameters["explicit_cardinality"] = LogicalType::UBIGINT;
+	fn.named_parameters["file_row_number"]      = LogicalType::BOOLEAN;
+
 	// fn.statistics = MultiFileFunction<FastLanesMultiFileInfo>::MultiFileScanStats;
-	fn.filter_pushdown = true;
-	fn.filter_prune    = true;
+	fn.get_row_id_columns   = FastLanesGetRowIdColumns;
+	fn.pushdown_expression  = FastLanesScanPushdownExpression;
+	fn.filter_pushdown      = true;
+	fn.filter_prune         = true;
+	fn.late_materialization = false;
 
 	ExtensionUtil::RegisterFunction(db, MultiFileReader::CreateFunctionSet(static_cast<TableFunction>(fn)));
 }
